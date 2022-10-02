41487240c87a4a3b4f9b5ac37089bfc734d3c930
Konstantsin <kostyannn1996@gmail.com>
2022-10-02 18:45:41 +0200

    fix: fix bugs with qoutes

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
-import { configureStore } from "@reduxjs/toolkit";
-import movieReducer from "../movieList/redux/MovieListSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import movieReducer from '../movieList/redux/MovieListSlice';
 
 export const store = configureStore({
   reducer: {
